69d7e0fda1bf47f1e2e79a72d2282c331cd56c2a
jocha <joch.arends@gmail.com>
2023-05-09 00:38:16 +0200

    working on week one

---
+++ Week01/Grade.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week01
+{
+    internal class Grade
+    {
+        private decimal value;
+        private bool frozen = false;
+        private readonly DateTime date;
+        private readonly int examCode;
+        private readonly string note;
+
+        public decimal Value
+        {
+            get => value;
+
+            set
+            {
+                if (!IsValidValue(value))
+                {
+                    throw new ArgumentException("Error: Invalid grade value.");
+                }
+                this.value = value;
+            }
+        }
+
+        public bool Frozen
+        {
+            get => frozen;
+
+            set
+            {
+                if (frozen && value == false)
+                {
+                    throw new ArgumentException("Error: Cannot unfreeze grade.");
+                }
+                this.frozen = value;
+            }
+        }
+
+        public DateTime Date => date;
+        public int ExamCode => examCode;
+        public string Note => note;
+
+        public Grade(decimal value, DateTime date, int examCode, string note = "")
+        {
+            Value = value;
+            this.date = date;
+            this.examCode = examCode;
+            this.note = note;
+        }
+
+        public Grade(decimal value, int examCode, string note = "") : this(value, DateTime.Now, examCode, note) 
+        { 
+        }
+
+        public override string ToString() => $"{examCode} on {date}: {value}";
+
+        private bool IsValidValue(decimal value)
+        {
+            const decimal minValue = 1.0m;
+            const decimal maxValue = 10.0m;
+            
+            if (value >= minValue && value <= maxValue)
+            {
+                return IsMultipleOf(value, 0.5m);
+            }
+            return false;
+        }
+
+        private bool IsMultipleOf(decimal value, decimal factor)
+        {
+            return value % factor == 0;
+        }
+    }
+}
---
+++ Week01/Student.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week01
+{
+    internal class Student
+    {
+        private string firstName;
+        private string lastName;
+        private readonly DateTime birthDate;
+        private readonly int studentNumber;
+        private readonly List<Grade> grades = new();
+
+        public string FirstName => firstName;
+        public string LastName => lastName;
+        public string FullName => $"{firstName} {lastName}";
+        public DateTime BirthDate => birthDate;
+        public int StudentNumber => studentNumber;
+
+        public Student(string firstName, string lastName, DateTime birthDate, int studentNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.studentNumber = studentNumber;
+        }
+
+        public void SetGrade(int examCode, decimal value)
+        {
+            if (IsResit(examCode))
+            {
+                GradesFor(examCode).First(grade => !grade.Frozen).Value = value;
+            }
+            else
+            {
+                grades.Add(new Grade(value, examCode));
+            }
+        }
+
+        public void PrintGrades()
+        {
+            foreach (Grade grade in grades.OrderBy(grade => grade.Date))
+            {
+                Console.WriteLine(grade);
+            }
+        }
+
+        public void PrintGrades(DateTime from, DateTime till)
+        {
+            Func<Grade, bool> isInPeriod = grade => grade.Date >= from && grade.Date <= till;
+
+            foreach (Grade grade in grades.Where(isInPeriod).OrderBy(grade => grade.Date))
+            {
+                Console.WriteLine(grade);
+            }
+        }
+
+        public List<Grade> GradesFor(int examCode)
+        {
+            return grades.Where(grade => grade.ExamCode == examCode).ToList();
+        }
+
+        public decimal GradePointAverage()
+        {
+            var examCodes = grades.Select(grade => grade.ExamCode).Distinct();
+            var finalGrades = examCodes.Select(examCode => FindHighestGrade(examCode));
+            return finalGrades.Average(grade => grade.Value);
+        }
+
+        public override string ToString() => $"{FullName} ({studentNumber})";
+
+
+        private bool IsResit(int examCode)
+        {
+            return grades.Any(grade => grade.ExamCode == examCode && !grade.Frozen);
+        }
+
+        private Grade FindGrade(int examCode)
+        {
+            return GradesFor(examCode).First(grade => !grade.Frozen);
+        }
+
+        private Grade FindHighestGrade(int examCode)
+        {
+            return GradesFor(examCode).OrderBy(grade => grade.Value).First();
+        }
+    }
+}
