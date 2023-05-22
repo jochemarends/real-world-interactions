fd9327ec9e783ade407c05165ded915662a5989b
Jochem Arends <contact@jochemarends.com>
2023-05-22 21:28:25 +0200

    Working on week three again

--- Week03/RPNCalculator.cs
+++ Week03/RPNCalculator.cs
@@ -5,7 +5,6 @@ using System.Runtime.Intrinsics.X86;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-using Week03;
 
 namespace Week03
 {
--- Week03/Token.cs
+++ Week03/Token.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Week02
+namespace Week03
 {
     internal enum TokenType
     {
